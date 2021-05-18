11a03988ef8cacbc856afd7b9f64cfdd4a8bd585
diogeneshas <diogenes.henrique.br@gmail.com>
2021-05-18 17:05:38 -0300

    V-for

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <Cliente v-bind:nome="nomeDoDiogenes" idade="36" v-bind:showIdade="true" />
     <Cliente nome="henrique" idade='16' v-bind:showIdade="false"/>
     <Cliente nome="almeida" idade='37' v-bind:showIdade="true"/>
+    <div v-for="(cliente,index) in clientes" v-bind:key="cliente.id">
+      <p>{{index}}</p>
+      {{cliente}}
+    </div>
   </div>
 </template>
 
@@ -15,7 +19,25 @@ export default {
   name: 'App',
   data() {
     return{
-      nomeDoDiogenes: "Diogenes henrique de Almeida Silva"
+      nomeDoDiogenes: "Diogenes henrique de Almeida Silva",
+      clientes: [{
+        id: 1,
+        nome: 'diogenes',
+        email: 'diogenes@gmail.com',
+        idade: 36
+      },
+      {
+        id: 2,
+        nome: 'henrique',
+        email: 'henrique@gmail.com',
+        idade: 16
+      },
+      {
+        id: 3,
+        nome: 'almeida',
+        email: 'almeida@gmail.com',
+        idade: 37
+      }]
     }
   },
   components: {
